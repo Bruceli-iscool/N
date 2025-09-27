e22f3580b552a3024ac9b3cc6c0ae79bb1e66c2f
Bruce Li <isanoob1897@outlook.com>
2025-09-27 09:55:49 -0700

    add variable support

--- src/n.java
+++ src/n.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class n {
+    protected static HashMap<String, Integer> vars = new HashMap<>();
     public static void main(String args[]) {
         System.out.println("Calculator v1.0 by Bruce Li\n(c) 2025 All Rights Reserved.\nNO WARRANTY IS PROVIDED OF ANYKIND!");
         repl();
@@ -8,8 +9,13 @@ public class n {
     public static void repl() {
         try {
             Scanner i = new Scanner(System.in);
+            boolean inDef = false;
+            String varName ="";
+            int currentVarValue = 0;
             while (true) {
                 String line = i.nextLine();
+                if (line == null) continue;
+                line = line.trim();
                 ArrayList<String> tokens = lex(line);
                 while (!tokens.isEmpty()) {
                     String current = get(tokens);
@@ -26,15 +32,33 @@ public class n {
                                 int y = Integer.parseInt(current);
                                 current = get(tokens);
                                 tokens = remove(tokens);
-                                System.out.println(executeOperation(x, y, operator));
+                                if (inDef) {
+                                    currentVarValue += executeOperation(x, y, operator);
+                                } else {
+                                    System.out.println(executeOperation(x, y, operator));
+                                }
                             } else {
                                 expectedIntError(current);
                             }
                         } else {
                             expectedIntError(current);
                         }
+                    } else if (current.matches("define")) {
+                        current = get(tokens);
+                        tokens = remove(tokens);
+                        if (current.matches("^[a-zA-Z_$][a-zA-Z0-9_$]*$")) {
+                            varName = current;
+                            inDef = true;
+                            currentVarValue = 0; 
+                        }
                     } else if (current.matches("quit")) {
+                        i.close();
                         System.exit(0);
+                    } else if (current.matches("-?\\d+")&&inDef) {
+                        currentVarValue += Integer.parseInt(current);
+                    } else if (current.matches("end")&&inDef) {
+                        inDef = false;
+                        vars.put(varName, currentVarValue);
                     }
                 }
             }
@@ -49,6 +73,10 @@ public class n {
         String p = line;
         for (int i = 0; i < p.length(); i++) {
             char c = p.charAt(i);
+            if (c == '-' && !ifString && z.isEmpty() && i + 1 < p.length() && Character.isDigit(p.charAt(i + 1))) {
+                z += c;
+                continue;
+            }
             switch (c) {
                 case '(': case ')': case ';': case '=': case '+': case '-': case '*': case '/':case '{': case '}':case ':':case '%':
                     if (!z.isEmpty()) {
